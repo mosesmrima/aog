984269729ca269ee22f53964d7aeea6a215f687d
mrima <mrimamss@gmail.com>
2025-07-02 22:06:55 +0300

    Upgrade Next.js and simplify config

--- next.config.js
+++ next.config.js
@@ -6,26 +6,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: { unoptimized: true },
-  experimental: {
-    esmExternals: false,
-  },
-  webpack: (config, { isServer }) => {
-    // Ignore optional WebSocket dependencies for client-side builds
-    if (!isServer) {
-      config.externals.push({
-        'bufferutil': 'bufferutil',
-        'utf-8-validate': 'utf-8-validate',
-      });
-    }
-    
-    // Disable minification temporarily to avoid chunk corruption
-    if (!isServer) {
-      config.optimization.minimize = false;
-    }
-    
-    return config;
-  },
 };
 
 module.exports = nextConfig;
